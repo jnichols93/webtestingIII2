697f0371c44c47aa3f896594e78179ec88b528c8
jnichols93 <51006450+jnichols93@users.noreply.github.com>
2019-10-15 15:34:32 -0500

    Update Controls.spec.js

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,33 @@
-// Test away!
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Controls from "./Controls";
+
+
+test("should show buttons that toggle state",() => {
+    expect(render(<Controls />)).toMatchSnapshot();
+})
+
+test("text on buttons toggles to reflect state", () => {
+
+})
+
+test("if gate is locked, close button is disabled", () => {
+    const { getByTestId } = render(<Controls />);
+    const lockButton = getByTestId(/lock/i);
+    const openButton = getByTestId(/open/i);
+
+    fireEvent.click(openButton);
+    fireEvent.click(lockButton);
+    expect(openButton.disabled).toEqual(false);
+}) 
+
+
+test ("locked button is disabled when gate is open", () => {
+    const { getByTestId } = render(<Controls />);
+    const openButton = getByTestId(/open/i);
+    const lockButton = getByTestId(/lock/i);
+
+    fireEvent.click(openButton);
+
+    expect(lockButton.disabled).toBe(true);
+})
